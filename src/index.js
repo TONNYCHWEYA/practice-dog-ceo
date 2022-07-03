82c2362ed5c6afc53a17231a4bc493451d581baa
TONNYCHWEYA <chweyatonny@gmail.com>
2022-07-03 06:19:50 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('%c HI', 'color: firebrick')
+const div = document.getElementById("dog")
+const ul = document.getElementById("dog-breeds")
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response => response.json())
+.then(data => {
+ 
+ 
+  data.message.forEach((image) => {
+    const img = document.createElement("img")
+    img.src = image
+    div.appendChild(img)
+  })
+})
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data =>{
+    console.log(data.message)
+    data.message.forEach((breed) => {
+      const li = document.createElement("li")
+      li.textContent = breed
+      ul.appendChild(li)
+    })
+  })
+
+})
